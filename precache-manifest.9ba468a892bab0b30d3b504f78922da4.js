86418bb0bed4ec29162ded638ac29349f3c81eb3
AntiINSA <douykhang2002@gmail.com>
2024-12-25 20:40:42 +0100

    Updates

---
+++ precache-manifest.9ba468a892bab0b30d3b504f78922da4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "765caf5012d144115fa220d7e4dd74ae",
+    "url": "/dlvt/index.html"
+  },
+  {
+    "revision": "3278c7a6fd46d2fd3ee0",
+    "url": "/dlvt/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "be3a0f4fa38086a9245d",
+    "url": "/dlvt/static/css/main.c753c63b.chunk.css"
+  },
+  {
+    "revision": "3278c7a6fd46d2fd3ee0",
+    "url": "/dlvt/static/js/2.de82250d.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/dlvt/static/js/2.de82250d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be3a0f4fa38086a9245d",
+    "url": "/dlvt/static/js/main.88da110d.chunk.js"
+  },
+  {
+    "revision": "3b84baf7a9411004fd35",
+    "url": "/dlvt/static/js/runtime-main.8b5f649e.js"
+  }
+]);
